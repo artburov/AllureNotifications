99db845c193439e67ba8ce7e75e8914b3881f659
Arthur Burov <art.burov@gmail.com>
2021-04-17 21:58:32 +0300

    added empty different type of tests

---
+++ src/test/java/OtherPositiveTests.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Other positive tests")
+public class OtherPositiveTests {
+    @Test
+    void some1Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some2Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some3Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some4Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some5Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some6Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some7Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some8Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some9Test() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/PositiveTests.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Positive tests")
+public class PositiveTests {
+    @Test
+    void some1Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some2Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some3Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some4Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some5Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some6Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some7Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some8Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some9Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some10Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some11Test() {
+        assertTrue(true);
+    }
+}
